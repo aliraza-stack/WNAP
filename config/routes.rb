cd0ccc55df1beb17ce7de29659d8ac53c702911b
Ali Raza <aliraza.yasin.714@gmail.com>
2021-10-20 16:06:10 +0500

    WNAP

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'home/contact'
-  get 'home/about'
+  root to: 'home#index'
+  get "contact", to: 'home#contact'
+  get "about", to: 'home#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
